25fe31bad67cb9d8707cbf071f379a5a6ae28c2c
AkifNaveed12 <akif27031@gmail.com>
2026-09-17 14:56:19 +0500

    binary  to  octal conversion

--- lab-02/Activity3_BinaryToOctal.java
+++ lab-02/Activity3_BinaryToOctal.java
@@ -1,5 +1,25 @@
-package lab-02;
-
+import  java.util.Scanner;
 public class Activity3_BinaryToOctal {
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // ask user to enter binary no
+        System.out.println("Binary to Octal: ");
+        System.out.println("Enter the number: ");
+
+        String binary = scanner.nextLine();
+
+        // convert binary to decimal
+        int decimal = Integer.parseInt(binary, 2);
+
+        // convert decimal to octal 
+        String octal = Integer.toOctalString(decimal);
+
+        // Display octal value
+        System.out.println(" octal value is : " + octal);
+
+        scanner.close();
+    }
+
 }
